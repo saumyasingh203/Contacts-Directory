46c5f67619ac2b0e8d67ece6882b71ec5443d042
Saumya Singh <saumya.singh203@gmail.com>
2021-03-15 23:26:05 +0530

    minor update

--- routes/auth.js
+++ routes/auth.js
@@ -4,13 +4,23 @@ const express = require('express');
 const router = express.Router();
 
 
-//@route  GET  api/users
+//@route  GET  api/auth
 //@desc   Get logged in user
 //@access Private
 router.get('/', (req, res) => {
 
     res.send('Get logged in user');
 });
+
+
+//@route  POST  api/auth
+//@desc   Auth user and get token
+//@access Public
+router.get('/', (req, res) => {
+
+    res.send('Log in User');
+});
+
 
 
 //have to write this otherwise it wont work
