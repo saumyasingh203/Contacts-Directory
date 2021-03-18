22620c557a7eb79d70217a56af5f4febfc7d5d65
Saumya Singh <saumya.singh203@gmail.com>
2021-03-18 09:34:13 +0530

    Routes completed

--- routes/auth.js
+++ routes/auth.js
@@ -16,7 +16,7 @@ router.get('/', (req, res) => {
 //@route  POST  api/auth
 //@desc   Auth user and get token
 //@access Public
-router.get('/', (req, res) => {
+router.post('/', (req, res) => {
 
     res.send('Log in User');
 });
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,1 +1,49 @@
 //Just CRUD functionality, read update etc
+//Will have 4 routes - Create read update delete
+
+const express = require('express');
+const router = express.Router();
+
+
+//@route  GET  api/contacts
+//@desc   Get all user's contacts
+//@access Private
+router.get('/', (req, res) => {
+
+    res.send('Get all contacts');
+});
+
+
+
+//@route  POST  api/contacts
+//@desc   Add new contact
+//@access Private
+router.post('/', (req, res) => {
+
+    res.send('Add contact');
+});
+
+
+//placeholder for what we wanna update /:id
+
+//@route  PUT  api/contacts/:id
+//@desc   Update contact
+//@access Private
+router.put('/:id', (req, res) => {
+
+    res.send('Update contact');
+});
+
+
+//@route  DELETE  api/contacts/:id
+//@desc   delete contact
+//@access Private
+router.delete('/:id', (req, res) => {
+
+    res.send('Delete contact');
+});
+
+
+
+//have to write this otherwise it wont work
+module.exports=router;
