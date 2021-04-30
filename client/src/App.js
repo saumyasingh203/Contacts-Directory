4bec1e0766a1f7ee0a390898b89b8d0ce26b01c2
Saumya Singh <saumya.singh203@gmail.com>
2021-04-30 19:05:45 +0530

    client site + crud

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Navbar />
+      My App
+    </div>
+  );
+};
+
+export default App;
