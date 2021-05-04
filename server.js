8de606ccdc428269b30ec09a550f21f4b33fc314
Saumya Singh <saumya.singh203@gmail.com>
2021-05-04 18:37:09 +0530

    deploy4

--- server.js
+++ server.js
@@ -4,20 +4,13 @@ const path = require("path");
 
 const app = express();
 
-//Connect Database
+// Connect Database
 connectDB();
 
-//init middleware
+// Init Middleware
 app.use(express.json({ extended: false }));
 
-//creating an end point so that we get something when server starts
-//we can send a file too res.file but here we send a json object
-
-//now we will make 3 different files for our routes
-// app.get('/', (req, res) => {res.json({msg: 'Welcome to the Contacts-Directory API by Saumya Singh'})})
-
-//Define routes
-//we want every backend route to start with /api/users
+// Define Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
@@ -27,13 +20,11 @@ if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
 
-  //* means anything which is not the above 3 routes
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
 }
 
-//while developing, use port 5000, .env.PORT is for after deployment
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
